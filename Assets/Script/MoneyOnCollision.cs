df9820357cf2d85c35852e0c441d1f4c671cb953
Christopher Maire <locura.madness@gmail.com>
2015-11-07 23:47:03 -0500

    Person collision whatever something

--- Assets/Script/MoneyOnCollision.cs
+++ Assets/Script/MoneyOnCollision.cs
@@ -10,7 +10,17 @@ public class MoneyOnCollision : MonoBehaviour {
         business = FindObjectOfType<Business>();
 	}
 	
-	void OnCollisionEnter2D()
+	void OnCollisionEnter2D(Collision2D c)
+    {
+        DoCollide();
+    }
+
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        DoCollide();
+    }
+
+    void DoCollide()
     {
         business.EarnMoney(Amount);
         FloatyTextManager.ShowFloatyMoney(Amount, Camera.main.WorldToScreenPoint(transform.position));
