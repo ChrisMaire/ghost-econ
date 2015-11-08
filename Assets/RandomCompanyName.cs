6f5ec9199ef84c878b6d8de2b727994f946241eb
Christopher Maire <locura.madness@gmail.com>
2015-11-08 01:33:31 -0500

    Business randomizer

---
+++ Assets/RandomCompanyName.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RandomCompanyName : MonoBehaviour {
+    public string[] Field1;
+    public string[] Field2;
+
+    InputField input;
+
+    void Awake() {
+        input = GetComponent<InputField>();
+        input.text = Field1[Random.Range(0, Field1.Length)] + " " + Field2[Random.Range(0, Field2.Length)];    
+	}
+}
